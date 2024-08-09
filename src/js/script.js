4d0587a3bbc5dfba79fb3c006e2d634b4a1c96b6
Paweł <p.grabczak@cognitionhub.com>
2024-08-09 17:23:26 +0200

    Task 8.3

--- src/js/script.js
+++ src/js/script.js
@@ -288,6 +288,31 @@ const select = {
     }
 
   }
+  
+  class Cart { //znow to samo!!! czytaj ze zrozumieniem i nie kombinuj, to musia działać!!!!!!!
+    constructor(element) {
+      const thisCart = this;
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log ('new Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+      
+      thisCart.dom.toggleTrigger.addEventListener('click', function() {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
 
   const app = {
     
@@ -305,6 +330,12 @@ const select = {
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -315,6 +346,7 @@ const select = {
       
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
