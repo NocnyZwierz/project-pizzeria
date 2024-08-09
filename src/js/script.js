02bfa59917571af3cf0d0c409be9d31ac6933822
Paweł <p.grabczak@cognitionhub.com>
2024-08-09 15:04:22 +0200

    Task 8.1

--- src/js/script.js
+++ src/js/script.js
@@ -61,12 +61,13 @@ const select = {
 
       thisProduct.renderInMenu();
       thisProduct.getElements();
+      thisProduct.initAmountWidget();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
       thisProduct.processOrder();
       console.log('new Product:', thisProduct)
     }
-
+    //moduł 8 doczytaj jeszcze o tym!!!! podstawa e-comers
     renderInMenu () {
       const thisProduct = this;
 
@@ -84,7 +85,8 @@ const select = {
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
-      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper); // matreiały poprowadziły jak dziecko za rączkę i do knajpy
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
     initAccordion(){
@@ -105,7 +107,7 @@ const select = {
       });
     }
 
-    initOrderForm () {
+    initOrderForm () { // nie kąbinuj bo potem nie działa, czytaj uważnie polecenia!!!!
       const thisProduct = this;
       console.log('initOrderFrom', thisProduct);
 
@@ -173,10 +175,91 @@ const select = {
           }
         }
       }
-    
+      price *= thisProduct.amountWidget.value;
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
+
+    initAmountWidget() {
+      const thisProduct = this;
+    
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function() {
+        thisProduct.processOrder();
+      });
+    }
+  }
+
+  class AmountWidget {
+    constructor (element) {
+      const thisWidget = this;
+
+      thisWidget.getElements(element);
+      const initialValue = thisWidget.input.value || settings.amountWidget.defaultValue;
+      thisWidget.setValue(initialValue);
+      thisWidget.initActions();
+
+      thisWidget.input.addEventListener('change', function() {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
+
+    getElements(element){
+      const thisWidget = this;
+    
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(newValue) {
+      const thisWidget = this;
+  
+      newValue = parseInt(newValue);
+
+      const minValue = settings.amountWidget.defaultMin;
+      const maxValue = settings.amountWidget.defaultMax;
+  
+      if(thisWidget.value !== newValue && !isNaN(newValue) && newValue >= minValue && newValue <= maxValue) {
+        thisWidget.value = newValue;
+        thisWidget.announce(); 
+      }
+  
+      thisWidget.input.value = thisWidget.value;
+    }
+
+    announce() {
+      const thisWidget = this;
+    
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
+    
+      thisWidget.element.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisWidget = this;
+  
+      thisWidget.input.addEventListener('change', function() {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+  
+      thisWidget.linkDecrease.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+  
+      thisWidget.linkIncrease.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+
   }
 
   const app = {
